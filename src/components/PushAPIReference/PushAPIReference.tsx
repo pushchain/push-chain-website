0281604106e6b86b2752a4e97365ede637767898
harshrajat <harsh@epns.io>
2025-06-29 22:58:14 +0400

    docs: enhance theme customization docs with usage examples and variable descriptions

--- src/components/PushAPIReference/PushAPIReference.tsx
+++ src/components/PushAPIReference/PushAPIReference.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 interface PushAPIReferenceProps {
   children: React.ReactNode;
-  title?: string;
   className?: string;
   showRequiredNotice?: boolean;
 }
