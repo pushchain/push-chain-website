4f35b1fce101275cb87c85a8823413dc57593acf
corlard3y <corlardey@gmail.com>
2025-07-11 15:14:21 +0100

    test: safari mediaquery changes

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,45 +1,92 @@
-// React + Web3 Essentials
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+//
+//
 import { useEffect, useState } from 'react';
 
 function useMediaQuery(query: string): boolean {
-  const getMatches = (query: string): boolean => {
-    // Prevents SSR issues
-    if (typeof window !== 'undefined') {
-      return window.matchMedia(query).matches;
-    }
-    return false;
+  const getMatches = (q: string): boolean => {
+    if (typeof window === 'undefined') return false;
+    return window.matchMedia(q).matches;
   };
 
   const [matches, setMatches] = useState<boolean>(getMatches(query));
 
-  function handleChange() {
-    setMatches(getMatches(query));
-  }
-
   useEffect(() => {
-    const matchMedia = window.matchMedia(query);
+    if (typeof window === 'undefined') return () => {};
 
-    // Triggered at the first client-side load and if query changes
-    handleChange();
+    const mql = window.matchMedia(query);
 
-    // Listen matchMedia
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange);
-    } else {
-      matchMedia.addEventListener('change', handleChange, { passive: true });
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
+
+    // For Safari compatibility: older versions only support addListener/removeListener
+    if (mql.addEventListener) {
+      mql.addEventListener('change', handleChange);
+    } else if (mql.addListener) {
+      mql.addListener(handleChange);
     }
 
+    // Always update state immediately in case initial value changed
+    setMatches(mql.matches);
+
     return () => {
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange);
-      } else {
-        matchMedia.removeEventListener('change', handleChange);
+      if (mql.removeEventListener) {
+        mql.removeEventListener('change', handleChange);
+      } else if (mql.removeListener) {
+        mql.removeListener(handleChange);
       }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
   return matches;
 }
 
 export default useMediaQuery;
+
+// // React + Web3 Essentials
+// import { useEffect, useState } from 'react';
+
+// function useMediaQuery(query: string): boolean {
+//   const getMatches = (query: string): boolean => {
+//     // Prevents SSR issues
+//     if (typeof window !== 'undefined') {
+//       return window.matchMedia(query).matches;
+//     }
+//     return false;
+//   };
+
+//   const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+//   function handleChange() {
+//     setMatches(getMatches(query));
+//   }
+
+//   useEffect(() => {
+//     const matchMedia = window.matchMedia(query);
+
+//     // Triggered at the first client-side load and if query changes
+//     handleChange();
+
+//     // Listen matchMedia
+//     if (matchMedia.addListener) {
+//       matchMedia.addListener(handleChange);
+//     } else {
+//       matchMedia.addEventListener('change', handleChange, { passive: true });
+//     }
+
+//     return () => {
+//       if (matchMedia.removeListener) {
+//         matchMedia.removeListener(handleChange);
+//       } else {
+//         matchMedia.removeEventListener('change', handleChange);
+//       }
+//     };
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [query]);
+
+//   return matches;
+// }
+
+// export default useMediaQuery;
