981138280be54ae03e742532a7fabb7460e1d753
corlard3y <corlardey@gmail.com>
2025-07-30 23:58:19 +0100

    update marquee links

--- src/config/HomeMediaList.ts
+++ src/config/HomeMediaList.ts
@@ -1,21 +1,21 @@
 export const MediaList = [
   {
-    url: 'https://cointelegraph.com/news/unstoppable-domains-launches-badge-gated-group-chat-feature-username-holders',
+    url: 'https://www.bnbchain.org/en/blog/push-protocol-launches-on-bnb-chain',
     text: 'We are thrilled to announce that Push Protocol, a leading decentralized communications protocol for Web3, has now launched on BNB Chain.',
     srcref: 'binance-chain',
   },
   {
-    url: 'https://www.coindesk.com/business/2022/04/14/ethereum-push-notification-service-raises-10m-at-131m-valuation/',
+    url: 'https://www.coindesk.com/tech/2024/01/11/protocol-latest-tech-news-crypto-blockchain',
     text: 'Protocol Village: Unstoppable Works With Push Protocol to Deliver Token-Gated Group Chats',
     srcref: 'coindesk',
   },
   {
-    url: 'https://thedefiant.io/unstoppable-domains-launches-token-gated-group-chats',
+    url: 'https://thedefiant.io/news/defi/unstoppable-domains-launches-token-gated-group-chats',
     text: 'Unstoppable Domains Launches Token Gated Group Chats. Collaboration with Push Protocol aims to fend off misinformation and scams.',
     srcref: 'defiant',
   },
   {
-    url: 'https://erc4337.mirror.xyz/ghX6mENwSgsNqLz7Gqlt940akHzYWZH4LRaRAgx77VI',
+    url: 'https://balajis.com/p/why-india-should-buy-bitcoin',
     text: 'User-controlled notifications. Two companies control push notifications to billions of smartphones: Apple and Google.',
     srcref: 'balaji',
   },
@@ -38,47 +38,47 @@ export const MediaList = [
 
 export const SecondMediaList = [
   {
-    url: 'https://cointelegraph.com/news/unstoppable-domains-launches-badge-gated-group-chat-feature-username-holders',
+    url: 'https://www.theblock.co/post/330507/protocol-push-layer-1',
     text: 'Web3 notifications protocol Push to launch its own Layer 1 chain',
     srcref: 'the-block',
   },
   {
-    url: 'https://www.coindesk.com/business/2022/04/14/ethereum-push-notification-service-raises-10m-at-131m-valuation/',
+    url: 'https://markets.businessinsider.com/news/currencies/push-unveils-push-chain-the-future-of-universal-blockchain-applications-1034129422',
     text: 'Push Unveils Push Chain: The Future of Universal Blockchain Applications',
     srcref: 'business-insider',
   },
   {
-    url: 'https://thedefiant.io/unstoppable-domains-launches-token-gated-group-chats',
+    url: 'https://cryptoslate.com/press-releases/push-unveils-push-chain-the-future-of-universal-blockchain-applications/',
     text: 'Push Unveils Push Chain: The Future of Universal Blockchain Applications.',
     srcref: 'chainwire',
   },
   {
-    url: 'https://erc4337.mirror.xyz/ghX6mENwSgsNqLz7Gqlt940akHzYWZH4LRaRAgx77VI',
+    url: 'https://www.benzinga.com/pressreleases/24/12/42458648/push-unveils-push-chain-the-future-of-universal-blockchain-applications',
     text: 'Push Unveils Push Chain: The Future of Universal Blockchain Applications',
     srcref: 'benzinga',
   },
   {
-    url: 'https://defiprime.com/ethereum-push-notification-service',
+    url: 'https://cryptobriefing.com/push-unveils-push-chain-the-future-of-universal-blockchain-applications/',
     text: 'Push Unveils Push Chain: The Future of Universal Blockchain Applications',
     srcref: 'crypto-briefing',
   },
   {
-    url: 'https://blog.ethereum.org/2020/08/19/esp-beyond-grants',
+    url: 'https://cryptoslate.com/push-protocol-launches-push-chain-to-unify-blockchain-communication-and-transactions/',
     text: 'Push Protocol launches Push Chain to unify blockchain communication and transactions',
     srcref: 'cryptoslate',
   },
   {
-    url: 'https://beincrypto.com/learn/web3-communication-networks/?utm_source=twitter&utm_medium=URL&utm_campaign=learn',
+    url: 'https://messari.io/project/ethereum-push-notification-service/profile',
     text: 'EPNS enables incentives and passive earnings for users receiving the decentralized notifications.',
     srcref: 'messari',
   },
   {
-    url: 'https://beincrypto.com/learn/web3-communication-networks/?utm_source=twitter&utm_medium=URL&utm_campaign=learn',
+    url: 'https://cointelegraph.com/news/hashing-it-out-a-case-for-web3-chat-apps-with-push-protocol-s-harsh-rajat',
     text: 'Harsh Rajat, founder and project lead of Push Protocol, explains the surge in the development of Web3 chat applications on the latest episode of Hashing It Out.',
     srcref: 'coin-telegraph',
   },
   {
-    url: 'https://beincrypto.com/learn/web3-communication-networks/?utm_source=twitter&utm_medium=URL&utm_campaign=learn',
+    url: 'https://cointelegraph.com/news/unstoppable-domains-launches-badge-gated-group-chat-feature-username-holders',
     text: 'Unstoppable Domains launches badge-gated group chat feature for username holders.',
     srcref: 'coin-telegraph',
   },
