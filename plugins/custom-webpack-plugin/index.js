4c281c2a757f3fc9f13b8554b9189a623c72a8b1
corlard3y <corlardey@gmail.com>
2025-08-01 17:07:24 +0100

    update webpack

--- plugins/custom-webpack-plugin/index.js
+++ plugins/custom-webpack-plugin/index.js
@@ -1,6 +1,8 @@
 // /* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+//
+const webpack = require('webpack');
 
 module.exports = function () {
   return {
