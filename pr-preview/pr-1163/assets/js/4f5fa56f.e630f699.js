f8153b9c21a99832598a5d3a45f5ed04b141371c
corlard3y <corlard3y@users.noreply.github.com>
2026-01-16 15:38:36 +0000

    Deploy preview for PR 1163 🛫

--- pr-preview/pr-1163/assets/js/4f5fa56f.e630f699.js
+++ pr-preview/pr-1163/assets/js/4f5fa56f.e630f699.js
@@ -1,7 +1,7 @@
 "use strict";
-(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[21429],{
+(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[87714],{
 
-/***/ 986479
+/***/ 604756
 (module) {
 
 module.exports = /*#__PURE__*/JSON.parse('{"blogTitle":"Blog","blogDescription":"Blog","totalPosts":433,"totalRecentPosts":4,"twitterId":"1869392237379092482"}');
--- pr-preview/pr-1163/assets/js/c9000f4d.a89e838d.js
+++ pr-preview/pr-1163/assets/js/c9000f4d.a89e838d.js
@@ -1,7 +1,7 @@
 "use strict";
-(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[87714],{
+(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[21429],{
 
-/***/ 604756
+/***/ 986479
 (module) {
 
 module.exports = /*#__PURE__*/JSON.parse('{"blogTitle":"Blog","blogDescription":"Blog","totalPosts":433,"totalRecentPosts":4,"twitterId":"1876998342112059691"}');
