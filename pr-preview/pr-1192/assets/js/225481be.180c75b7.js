b6f8f367dc0c6752e1e36714724f50e964004a00
corlard3y <corlard3y@users.noreply.github.com>
2026-02-27 19:31:09 +0000

    Deploy preview for PR 1192 🛫

--- pr-preview/pr-1192/assets/js/225481be.180c75b7.js
+++ pr-preview/pr-1192/assets/js/225481be.180c75b7.js
@@ -1,7 +1,7 @@
 "use strict";
-(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[6276],{
+(self["webpackChunkpush_chain_website"] = self["webpackChunkpush_chain_website"] || []).push([[4426],{
 
-/***/ 127476
+/***/ 300545
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
@@ -10,35 +10,34 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   contentTitle: () => (/* binding */ contentTitle),
 /* harmony export */   "default": () => (/* binding */ MDXContent),
 /* harmony export */   frontMatter: () => (/* binding */ frontMatter),
-/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_blog_site_blog_2024_12_09_consumer_apps_that_can_be_built_on_push_chain_index_md_b93_json__WEBPACK_IMPORTED_MODULE_0__),
+/* harmony export */   metadata: () => (/* reexport default export from named module */ _site_docusaurus_docusaurus_plugin_content_blog_blog_site_blog_2024_12_09_why_push_chain_index_md_6fe_json__WEBPACK_IMPORTED_MODULE_0__),
 /* harmony export */   toc: () => (/* binding */ toc)
 /* harmony export */ });
-/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_blog_site_blog_2024_12_09_consumer_apps_that_can_be_built_on_push_chain_index_md_b93_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(937596);
+/* harmony import */ var _site_docusaurus_docusaurus_plugin_content_blog_blog_site_blog_2024_12_09_why_push_chain_index_md_6fe_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(973396);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(474848);
 /* harmony import */ var _mdx_js_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(28453);
 
 
 const frontMatter = {
-	slug: 'consumer-apps-that-can-be-built-on-push-chain',
-	title: 'Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain',
+	slug: 'why-push-chain',
+	title: 'Why Push Chain is the Missing Layer in Web3',
 	authors: [
-		'push'
+		'harsh'
 	],
 	image: './cover-image.webp',
-	description: 'Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain',
-	text: 'Discover how Push Chain enables developers to build consumer apps that work across all blockchains. From AI agents to social platforms, explore real-world use cases that are only possible with universal shared state.',
+	description: 'How a shared-state L1 can heal web3’s fragmentation and unleash web3’s true potential.',
+	text: 'Web3 is broken. Users juggle 30+ chains, developers rebuild apps for each ecosystem, and fragmentation kills adoption. Learn why Push Chain\'s shared-state architecture is the missing piece to finally unify blockchain.',
 	tags: [
 		'Product',
 		'Key Features',
-		'Ecosystem',
-		'Push 101'
+		'Thought Leadership'
 	],
-	twitterId: '1881607795570860403'
+	twitterId: '1869392237379092482'
 };
 const contentTitle = undefined;
 
 const assets = {
-"image": (__webpack_require__(22722)/* ["default"] */ .A),
+"image": (__webpack_require__(337977)/* ["default"] */ .A),
 "authorsImageUrls": [undefined],
 };
 
@@ -54,8 +53,8 @@ function _createMdxContent(props) {
   };
   return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.p, {
     children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_components.img, {
-      alt: "Cover Image of Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain",
-      src: (__webpack_require__(929021)/* ["default"] */ .A) + "",
+      alt: "Cover Image of Why Push Chain",
+      src: (__webpack_require__(294164)/* ["default"] */ .A) + "",
       width: "1600",
       height: "900"
     })
@@ -78,23 +77,23 @@ function MDXContent(props = {}) {
 
 /***/ },
 
-/***/ 22722
+/***/ 337977
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/cover-image-5cd699e8baeab8015a6f9597167571ea.webp");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/cover-image-3b05984f165fa3633b109a33eefd52d2.webp");
 
 /***/ },
 
-/***/ 929021
+/***/ 294164
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   A: () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/cover-image-5cd699e8baeab8015a6f9597167571ea.webp");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "assets/images/cover-image-3b05984f165fa3633b109a33eefd52d2.webp");
 
 /***/ },
 
@@ -193,10 +192,10 @@ function MDXProvider(properties) {
 
 /***/ },
 
-/***/ 937596
+/***/ 973396
 (module) {
 
-module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/push-chain-website/pr-preview/pr-1192/blog/consumer-apps-that-can-be-built-on-push-chain","source":"@site/blog/2024-12-09-consumer-apps-that-can-be-built-on-push-chain/index.md","title":"Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain","description":"Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain","date":"2024-12-09T00:00:00.000Z","tags":[{"inline":true,"label":"Product","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/product"},{"inline":true,"label":"Key Features","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/key-features"},{"inline":true,"label":"Ecosystem","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/ecosystem"},{"inline":true,"label":"Push 101","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/push-101"}],"readingTime":4.54,"hasTruncateMarker":true,"authors":[{"name":"Push Chain","url":"https://x.com/PushChain","page":{"permalink":"/push-chain-website/pr-preview/pr-1192/blog/authors/push"},"imageURL":"/push-chain-website/pr-preview/pr-1192/assets/blog/authors/authorpush.png","key":"push"}],"frontMatter":{"slug":"consumer-apps-that-can-be-built-on-push-chain","title":"Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain","authors":["push"],"image":"./cover-image.webp","description":"Consumer Apps 🫶 Push Chain - Innovations that can be built on Push Chain","text":"Discover how Push Chain enables developers to build consumer apps that work across all blockchains. From AI agents to social platforms, explore real-world use cases that are only possible with universal shared state.","tags":["Product","Key Features","Ecosystem","Push 101"],"twitterId":"1881607795570860403"},"unlisted":false,"prevItem":{"title":"Evolution of Push","permalink":"/push-chain-website/pr-preview/pr-1192/blog/evolution-of-push"},"nextItem":{"title":"Understand Proof of Stake (POS) - Part 1","permalink":"/push-chain-website/pr-preview/pr-1192/blog/understand-proof-of-stake"}}');
+module.exports = /*#__PURE__*/JSON.parse('{"permalink":"/push-chain-website/pr-preview/pr-1192/blog/why-push-chain","source":"@site/blog/2024-12-09-why-push-chain/index.md","title":"Why Push Chain is the Missing Layer in Web3","description":"How a shared-state L1 can heal web3’s fragmentation and unleash web3’s true potential.","date":"2024-12-09T00:00:00.000Z","tags":[{"inline":true,"label":"Product","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/product"},{"inline":true,"label":"Key Features","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/key-features"},{"inline":true,"label":"Thought Leadership","permalink":"/push-chain-website/pr-preview/pr-1192/blog/tags/thought-leadership"}],"readingTime":5.65,"hasTruncateMarker":true,"authors":[{"name":"Harsh Rajat","title":"Co-Founder","url":"https://twitter.com/harshrajat","page":{"permalink":"/push-chain-website/pr-preview/pr-1192/blog/authors/harsh"},"imageURL":"/push-chain-website/pr-preview/pr-1192/assets/blog/authors/authorharsh.png","key":"harsh"}],"frontMatter":{"slug":"why-push-chain","title":"Why Push Chain is the Missing Layer in Web3","authors":["harsh"],"image":"./cover-image.webp","description":"How a shared-state L1 can heal web3’s fragmentation and unleash web3’s true potential.","text":"Web3 is broken. Users juggle 30+ chains, developers rebuild apps for each ecosystem, and fragmentation kills adoption. Learn why Push Chain\'s shared-state architecture is the missing piece to finally unify blockchain.","tags":["Product","Key Features","Thought Leadership"],"twitterId":"1869392237379092482"},"unlisted":false,"prevItem":{"title":"Understand Proof of Stake (POS) - Part 1","permalink":"/push-chain-website/pr-preview/pr-1192/blog/understand-proof-of-stake"}}');
 
 /***/ }
 
