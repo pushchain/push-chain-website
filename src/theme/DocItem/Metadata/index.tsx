63176ac067d5e74bae88a6c946605a85da21b169
corlard3y <corlardey@gmail.com>
2026-05-07 10:23:02 +0100

    update preview

--- src/theme/DocItem/Metadata/index.tsx
+++ src/theme/DocItem/Metadata/index.tsx
@@ -1,15 +1,26 @@
-import React, {type ReactNode} from 'react';
-import {PageMetadata} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';
+import React, { type ReactNode } from 'react';
+import Head from '@docusaurus/Head';
+import { PageMetadata } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
 
 export default function DocItemMetadata(): ReactNode {
-  const {metadata, frontMatter, assets} = useDoc();
+  const { metadata, frontMatter, assets } = useDoc();
+  const image = assets.image ?? frontMatter.image;
   return (
-    <PageMetadata
-      title={metadata.title}
-      description={metadata.description}
-      keywords={frontMatter.keywords}
-      image={assets.image ?? frontMatter.image}
-    />
+    <>
+      <PageMetadata
+        title={metadata.title}
+        description={metadata.description}
+        keywords={frontMatter.keywords}
+        image={image}
+      />
+      {image && (
+        <Head>
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:site' content='@PushChain' />
+          <meta name='twitter:creator' content='@PushChain' />
+        </Head>
+      )}
+    </>
   );
 }
